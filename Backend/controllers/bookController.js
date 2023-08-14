e6897cbbee22de74d281eaf1e4bd4d9dd84003f1
Paul Leblanc <azikx4qg@gmail.com>
2023-08-14 17:46:16 +0200

    test to resolve

--- Backend/controllers/bookController.js
+++ Backend/controllers/bookController.js
@@ -1,4 +1,4 @@
-const Book = require("../models/Book.js");
+const Book = require(path.resolve(__dirname, "..", "models", "Book.js"));
 const fs = require("fs");
 
 exports.getAllBooks = (req, res) => {
